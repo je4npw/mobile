93893ae149e86adad4284eaa320772f86df2625a
je4npw <je4n.pw@gmail.com>
2022-11-08 16:58:34 -0300

    Construção do hooke e preparativos para oauth

--- App.tsx
+++ App.tsx
@@ -1,24 +1,32 @@
-import { NativeBaseProvider, StatusBar } from 'native-base'
-import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium } from '@expo-google-fonts/roboto'
-import { Loading } from './src/components/Loading'
-import { SignIn } from './src/screens/SignIn'
-import { THEME } from './src/styles/theme'
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { Loading } from "./src/components/Loading";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { SignIn } from "./src/screens/SignIn";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
-    Roboto_500Medium
-  })
+    Roboto_500Medium,
+  });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
-
